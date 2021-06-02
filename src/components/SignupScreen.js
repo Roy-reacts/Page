3db79f94da3f173419560a6f60cae808183dc1e3
Roy-reacts <83531710+Roy-reacts@users.noreply.github.com>
2021-06-03 01:36:45 +0600

    Update

--- src/components/SignupScreen.js
+++ src/components/SignupScreen.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import "../App.css";
+import React,{ useEffect, useState } from "react";
+const Login = ({submitForm}) => {
+    const {handleChange,handleFormSubmit,values,errors}= useForm( submitForm);
 
 const SignupScreen = () => {
     return (
         <div className ="container">
         <div className ="app-wrapper">
-        <h1 className ="form-success">Account Created</h1>
         </div>
-            
+       
+        <div>
+        <header className="app">Amazon</header>
+        <h2>
+        Sign-In
+        </h2>
+        </div>
+        <form className = "form-wrapper">
+        <div className ="name">
+        <label className ="label">Your Name</label>
+        <input className="input" type="text" name="fullname" value={values.fullname} onChange={handleChange}/>
+        {errors.fullname && <p className="error">{errors.fullname}</p>}  
+
+        </div>
+        <div>
+        <button className="submit" onClick={handleFormSubmit}>Continue</button>
+        </div>
+        <div>By continuing, you agree to Amazon's Conditions of Use and Privacy Notice.
+        </div>
+        </form>
+        <ul><li>Need help?</li></ul>
         </div>
     );
 };
